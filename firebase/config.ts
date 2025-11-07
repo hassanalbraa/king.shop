2a1fc027a887100a9100f700c995ab17e096751c
hassanalbraa <162953992+hassanalbraa@users.noreply.github.com>
2025-11-07 14:13:51 +0200

    Add files via upload

---
+++ firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-268985317-a9f66",
+  "appId": "1:2527322905:web:1bf2de0afff05dac8d9b88",
+  "apiKey": "AIzaSyAUyev7HRCYvZl8cL4Q-wc-2e291v6zup8",
+  "authDomain": "studio-268985317-a9f66.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "2527322905"
+};
